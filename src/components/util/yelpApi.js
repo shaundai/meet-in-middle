6b616b872bc1039739a308b31eb1dd8b8fdce0af
Shaundai Person <shaundaiperson@gmail.com>
2020-10-14 23:18:38 -0400

    add endpoint

--- src/components/util/yelpApi.js
+++ src/components/util/yelpApi.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 const path = "http://localhost:3001"
 
 const Yelp = {
-    getBusinessesAndSort(){
-        return axios.get(`${path}/api/team/id/${userId}`)
+    getBusinessesAndSort(sortby){
+        return axios.get(`${path}/api/yelp/${sortby}?term=${searchTerm}&location=${searchLocation}`)
     }
 }
+
+export default Yelp
