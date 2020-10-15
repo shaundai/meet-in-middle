1d69223c183e638581db2d469ee31061a167d3d1
Shaundai Person <shaundaiperson@gmail.com>
2020-10-14 23:20:00 -0400

    add params to endpoint

--- src/components/util/yelpApi.js
+++ src/components/util/yelpApi.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = "http://localhost:3001"
 
 const Yelp = {
-    getBusinessesAndSort(sortby){
+    getBusinessesAndSort(sortby, searchTerm, searchLocation){
         return axios.get(`${path}/api/yelp/${sortby}?term=${searchTerm}&location=${searchLocation}`)
     }
 }
