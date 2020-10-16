7ad896f6dcf1a376d4d244be0771fb699fe70eae
Shaundai Person <shaundaiperson@gmail.com>
2020-10-15 20:51:13 -0400

    make function in yelp api work

--- src/components/util/yelpApi.js
+++ src/components/util/yelpApi.js
@@ -3,8 +3,9 @@ const axios = require('axios');
 const path = "http://localhost:3001"
 
 const Yelp = {
-    getBusinessesAndSort(sortby, searchTerm, searchLocation){
-        return axios.get(`${path}/api/yelp/${sortby}?term=${searchTerm}&location=${searchLocation}`)
+    //returns list of businesses after search by term, location, and a sorting method
+    getBusinessesAndSort(searchTerm, searchLocation, sortBy){
+        return axios.get(`${path}/api/yelp/businesses/list?searchterm=${searchTerm}&searchlocation=${searchLocation}&sortby=${sortBy}`)
     }
 }
 
