4b10f63592cdb9969aacb73820f3fe6bc59e55ab
Shaundai Person <shaundaiperson@gmail.com>
2020-10-18 00:16:05 -0400

    add input functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const App = () => {
+  const [startAndEnd, setStartAndEnd] = useState({start: '', end: ''})
+
   return (
-    <div>
-      <input></input>
-      <input></input>
-    </div>
+    <AppContainer>
+      <input type="text" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></input>
+      <input type="text" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></input>
+    </AppContainer>
   );
 }
 
 export default App;
 
-
+const AppContainer = styled.div`
+`
