fcd3c4e7f3127728a1a1573de5bebbe82a2b4f20
Shaundai Person <shaundaiperson@gmail.com>
2020-10-17 23:42:27 -0400

    add distance function in google service

--- src/components/util/googleApi.js
+++ src/components/util/googleApi.js
@@ -4,6 +4,14 @@ const path = "http://localhost:3001"
 
 const Google = {
 
+    //finds travel distance and time to get to two locations based on recommended route on roads
+    //start and end locations should be either lat/long separated by a comma or words separated by + signs
+    findDistanceBetweenTwoLocations(startLocation, endLocation){
+        return axios.get(`${path}/api/google/distance?searchorigins=${startLocation}&searchdestinations=${endLocation}`)
+    },
+
+    
+
 }
 
 export default Google
