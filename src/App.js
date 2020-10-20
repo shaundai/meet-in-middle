70534b381a8ac1aae6254cbf76b920f63c7a2150
Shaundai Person <shaundaiperson@gmail.com>
2020-10-20 18:14:01 -0400

    style form

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ const App = () => {
 
   return (
     <AppContainer>
-      <input type="text" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></input>
-      <input type="text" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></input>
+
+      <FormContainer>
+        <InputBox type="text" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></InputBox>
+        <InputBox type="text" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></InputBox>
+        <InputBox type="submit" value="Submit" />
+      </FormContainer>
     </AppContainer>
   );
 }
@@ -15,4 +19,16 @@ const App = () => {
 export default App;
 
 const AppContainer = styled.div`
+display: flex;
+width: 100%;
+`
+
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+`
+
+const InputBox = styled.input`
+display: flex;
 `
