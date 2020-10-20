815e580e1de334be3bf9ae3990042b71f310682a
Shaundai Person <shaundaiperson@gmail.com>
2020-10-20 18:59:08 -0400

    add category dropdown

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const App = () => {
-  const [startAndEnd, setStartAndEnd] = useState({start: '', end: ''})
+  const [startAndEnd, setStartAndEnd] = useState({start: '', end: '', locationType: 'All'})
 
   return (
     <AppContainer>
 
       <FormContainer>
+        <label>
+          First Address/City:
         <InputBox type="text" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></InputBox>
+        </label>
+        <label>
+          Second Address/City:
         <InputBox type="text" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></InputBox>
-        <InputBox type="submit" value="Submit" />
+        </label>
+        <label>
+          Meeting Location Type:
+          <select value={startAndEnd.locationType} onChange={e => {setStartAndEnd({...startAndEnd, locationType: e.target.value})}}>
+            <option value="All">Any</option>
+            <option value="food">Restaurant/Bar</option>
+            <option value="active">Things to Do</option>
+            <option value="shopping">Shopping</option>
+          </select>
+        </label>
+        
+        Any
+        Restaurant
+        <SubmitButton type="submit" value="Submit" />
+
+
       </FormContainer>
     </AppContainer>
   );
@@ -30,5 +50,8 @@ margin: 0 auto;
 `
 
 const InputBox = styled.input`
+display: flex;
+`
+const SubmitButton = styled.input`
 display: flex;
 `
