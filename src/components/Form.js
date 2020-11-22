36aebae0eed008f1ecef2ef4df038701abdc94f5
Shaundai Person <shaundaiperson@gmail.com>
2020-11-22 11:49:30 -0500

    add styling to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,27 +12,34 @@ const Form = () => {
             <AddressesContainer>
 
                 <Label>
-                First Address/City:
-                <InputBox type="text" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></InputBox>
+                First Location:
+                <InputBox type="text" placeholder="ex. 123 Sesame Street or Boston, MA" value={startAndEnd.start} onChange={e => {setStartAndEnd({...startAndEnd, start: e.target.value})}}></InputBox>
                 </Label>
 
                 <Label>
-                Second Address/City:
-                <InputBox type="text" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></InputBox>
+                Second Location:
+                <InputBox type="text" placeholder="ex. 123 Sesame Street or Boston, MA" value={startAndEnd.end} onChange={e => {setStartAndEnd({...startAndEnd, end: e.target.value})}}></InputBox>
                 </Label>
+
             </AddressesContainer>
 
-            <Label>
-              Meeting Location Type:
-              <SelectBox value={startAndEnd.locationType} onChange={e => {setStartAndEnd({...startAndEnd, locationType: e.target.value})}}>
-                <option value="All">Any</option>
-                <option value="food">Restaurant/Bar</option>
-                <option value="active">Things to Do</option>
-                <option value="shopping">Shopping</option>
-              </SelectBox>
-            </Label>
-    
-            <SubmitButton type="submit" value="Submit" />
+            <LocationTypeContainer>
+
+                <Label>
+                Location Type (optional):
+                <SelectBox value={startAndEnd.locationType} onChange={e => {setStartAndEnd({...startAndEnd, locationType: e.target.value})}}>
+                    <option value="All">Any</option>
+                    <option value="food">Restaurant/Bar</option>
+                    <option value="active">Things to Do</option>
+                    <option value="shopping">Shopping</option>
+                </SelectBox>
+                </Label>
+
+            </LocationTypeContainer>
+
+            <SubmitContainer>
+                <SubmitButton type="submit" value="Find A Place" />
+            </SubmitContainer>
     
     
           </FormContainer>
@@ -51,7 +58,6 @@ flex-direction: column;
 align-items: center;
 margin: 0 auto;
 font-family: Montserrat;
-border: 1px solid black;
 `
 
 const Label = styled.label`
@@ -62,8 +68,8 @@ flex-direction: column;
 
 const AddressesContainer = styled.div`
 display: flex;
-justify-content: space-evenly;
-width: 100%;
+width: 80%;
+justify-content: space-between;
 margin: 0 auto;
 padding: 40px 0;
 font-family: Montserrat;
@@ -74,31 +80,47 @@ display: flex;
 background-color: ${Colors.offWhite};
 border-radius: 8px;
 height: 30px;
-width: 280px;
+width: 320px;
 margin-top: 10px;
+padding: 0 8px;
+font-family: Montserrat;
+font-size: .9em;
+`
+
+const LocationTypeContainer = styled.div`
+display: flex;
+width: 80%;
+justify-content: center;
+margin-top: 5px;
 `
 
 const SelectBox = styled.select`
+margin-top: 10px;
 background-color: ${Colors.offWhite};
 padding: 0 8px;
 border-radius: 8px;
 font-family: Montserrat;
+font-size: .9em;
 color: ${Colors.mainBlack};
-height: 30px;
-width: 280px;
-margin-top: 10px;
+height: 35px;
+width: 230px;
 border-width: 2px;
 border-style: inset;
 border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
 `
 
+const SubmitContainer = styled.div`
+display: flex;
+`
+
 const SubmitButton = styled.input`
-padding: 5px;
+margin: 30px 0 20px;
+padding: 5px 7px;
 width: 120px;
 border-width: 1px;
 border-radius: 20px;
 font-weight: bold;
-font-size: 1em;
+font-size: .9em;
 font-family: Montserrat;
 background-color: ${Colors.mainPurple};
 color: ${Colors.offWhite};
