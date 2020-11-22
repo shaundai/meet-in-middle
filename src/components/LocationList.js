47ee0dd8023f79f9f606eb1079c7d3008bdb7492
Shaundai Person <shaundaiperson@gmail.com>
2020-11-22 11:53:38 -0500

    add styling to location list

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Colors } from './util/Colors'
+
 
 const LocationList = () => {
         return (
-            <div>
+            <LocationListContainer>
                 Map of list of locations
-            </div>
+            </LocationListContainer>
         )
 }
 
 export default LocationList
+
+const LocationListContainer = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    border: 1px solid black;
+`
