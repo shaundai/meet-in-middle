06c341ba05aba3e285dbd63e9b7ac225a704eef1
Shaundai Person <shaundaiperson@gmail.com>
2020-11-25 16:58:54 -0500

    try to fix error in google api call

--- src/components/AddressFormContainer.js
+++ src/components/AddressFormContainer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-final-form'
 
 import AddressForm from './AddressForm'
@@ -10,7 +10,8 @@ const AddressFormContainer = () => {
     const onSubmit = async (values) => {
         try {
             const distanceBetween = await findDistanceBetweenTwoLocations(values.firstLocation, values.secondLocation)
-            window.alert(JSON.stringify(distanceBetween, 0, 2))
+            return distanceBetween
+            console.log(distanceBetween)
         }
         catch(err){
           console.log(`My error code is ${err.status}.  I errored out bc ${err}`)
--- src/components/util/googleApi.js
+++ src/components/util/googleApi.js
@@ -12,5 +12,5 @@ export default Google
     //finds travel distance and time to get to two locations based on recommended route on roads
     //start and end locations should be either lat/long separated by a comma or words separated by + signs
 export const findDistanceBetweenTwoLocations = async (startLocation, endLocation) => {
-        return axios.get(`${path}/api/google/distance?searchorigins=${startLocation}&searchdestinations=${endLocation}`)
+        return (await axios.get(`${path}/api/google/distance?searchorigins=${startLocation}&searchdestinations=${endLocation}`)).data.data
     }
