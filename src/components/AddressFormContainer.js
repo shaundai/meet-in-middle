28ec6b711086bb4044f9ae173bc66be80b7e67da
Shaundai Person <shaundaiperson@gmail.com>
2020-11-25 16:36:33 -0500

    install axios

--- src/components/AddressFormContainer.js
+++ src/components/AddressFormContainer.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Form } from 'react-final-form'
 
 import AddressForm from './AddressForm'
+import { Yelp } from './util/yelpApi'
 
 const AddressFormContainer = () => {
 
     const onSubmit = async values => {
-        window.alert(JSON.stringify(values, 0, 2))
+        window.alert(JSON.stringify(values.locationType, 0, 2))
     }
     
         return (
